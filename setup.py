cb03131e225fba38140afdd8b375a0eb5bccd140
Anand Kumria <akumria@acm.org>
2011-03-01 04:19:19 +0000

    Specify more of the package of django-merchant

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='django-merchant',
       version='0.01',
-      packages=['billing', 'billing.utils'],
+      packages=['billing', 'billing.forms', 'billing.gateways.eway_gateway.eway_api', 'billing.management.commands', 'billing.models', 'billing.templatetags', 'billing.utils'],
      )
